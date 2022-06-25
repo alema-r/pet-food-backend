70dc8fc486da3e003daeecbde60b224d2fa0883b
alema-r <alessandromarcolini99@gmail.com>
2022-06-25 18:02:17 +0200

    minor changes

--- environment.d.ts
+++ environment.d.ts
@@ -1,21 +1,21 @@
-import { Jwt, JwtPayload } from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
 
 declare global {
-    // necessary to use process.env variables
-    namespace NodeJS {
-        interface ProcessEnv {
-            JWT_SECRET: string;
-            NODE_ENV: 'development' | 'production';
-            PORT?: string;
-            PWD: string;
-            PGDATABASE: string,
-            PGUSER: string,
-            PGPASS: string,
-            PGHOST: string,
-        }
+  // necessary to use process.env variables
+  namespace NodeJS {
+    interface ProcessEnv {
+      JWT_SECRET: string;
+      NODE_ENV: 'development' | 'production';
+      PORT?: string;
+      PWD: string;
+      PGDATABASE: string,
+      PGUSER: string,
+      PGPASS: string,
+      PGHOST: string,
     }
-    // to add properties to `express.Request`
-    namespace Express {
+  }
+  // to add properties to `express.Request`
+  namespace Express {
     interface Request {
       user: JwtPayload;
     }
@@ -23,4 +23,4 @@ declare global {
 }
 
 // to treat this script as module
-export{}
+export { }
